a3f794bb8b93fa7f42b7b0d92b9a29ab83928baa
tony nolan <tonynolan313@gmail.com>
2020-09-24 09:38:12 -0600

    fixed spelling errors

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"]
+var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+","<",">",";",":"]
 // function to prompt user for password options
 function passwordOptions() {
     var length = parseInt(prompt("Pick between 8 and 128 characters for password"));// prompt asking user to pick a number between 8 and 128
     if (length < 8) {
-        alert("please enter 8 or more carters!")
+        alert("please enter 8 or more characters")
         return
     }
     if (length > 128) {
