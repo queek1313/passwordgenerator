797d86be99d5ca02b89e156fd9ca979152b69083
tony nolan <tonynolan313@gmail.com>
2020-09-24 09:16:52 -0600

    code to script

---
+++ script.js
@@ -0,0 +1,5 @@
+// make four variables of lower case upper case and special carters and numbers.  these will be arrays holding your options.
+var numbers = ["1","2","3","4","5","6","7","8","9","0"]
+var lowerCase =[ "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var upperCase =[ "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var specialCharacter = ["!","@","#","$","%","^","&","*","(",")"]
